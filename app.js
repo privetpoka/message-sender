4a5eb741ba97418fa34471b5e0d6ec55a8f069ee
privetpoka <fififinger@gmail.com>
2017-02-26 00:13:25 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,43 @@
+/**
+ * Created by umadbro on 25/02/17.
+ */
+/**
+ * Created by umadbro on 21/02/17.
+ */
+var nodemailer = require ('nodemailer');
+
+var transporter = nodemailer.createTransport({
+
+    service: 'Gmail',
+    auth: {
+
+        user: 'yuradragon05@gmail.com',
+        pass: 'benjake10'
+    }
+});
+var i=0;
+var subject = 'Papa<parshkin67@mail.ru>';
+var arr = new Array("PAPA", "MISHA", "OTEC");
+for(var i=0; i<arr.length; i++) {
+
+    var message = '<b> hello,'+arr[i]+'</b>';
+    var mailOptions = {
+
+        from: 'Ura <yuradragond05@gmail.com>',
+        to: subject,
+        subject: arr[i],
+        text: 'hello darkness my old friend',
+        html: message
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+
+            console.log(i + info.response+ ' '+ info.messageId);
+        }
+
+    });
+}
+
